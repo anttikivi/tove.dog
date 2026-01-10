71139ec049a5d237d4338ec25255a347179c54fa
Antti Kivi <antti@anttikivi.com>
2026-01-10 18:29:17 +0200

    style: change Stylelint rules and fix the errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -9,5 +9,16 @@ export default {
             },
         ],
         "color-hex-length": "long",
+        "custom-property-pattern": null,
+        "custom-property-empty-line-before": null,
+        "declaration-property-value-no-unknown": [
+            true,
+            {
+                ignoreProperties: {
+                    "/.+/": ["/^--spacing\\(.+\\)/"],
+                },
+            },
+        ],
+        "import-notation": null,
     },
 };
