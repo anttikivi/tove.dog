726837d9aa1ac49e24c4ebad745134cd7c5fe149
Antti Kivi <antti@anttikivi.com>
2026-01-10 15:26:40 +0200

    feat: add a home page

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,12 @@
 export default {
     extends: ["stylelint-config-standard", "stylelint-config-html"],
     rules: {
+        "at-rule-no-unknown": [
+            true,
+            {
+                ignoreAtRules: ["theme", "source", "utility", "variant", "custom-variant", "apply", "reference"],
+            },
+        ],
         "color-hex-length": "long",
     },
 };
