ffec6d4b77b940e3fda560a7cd5c4ce3bece0e63
Antti Kivi <antti@anttikivi.com>
2026-01-10 12:59:57 +0200

    build: add Tailwind

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,29 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 
 // https://astro.build/config
 export default defineConfig({
     site: import.meta.env.PROD ? "https://tove.dog" : "http://localhost:4321",
+    trailingSlash: "always",
+    output: "static",
+    compressHTML: import.meta.env.PROD,
+    vite: {
+        plugins: [tailwindcss()],
+        css: {
+            transformer: "lightningcss",
+            lightningcss: {
+                targets: browserslistToTargets(browserslist(">= 0.01%, last 2 versions, Firefox ESR, not dead")),
+            },
+        },
+        build: {
+            cssMinify: "lightningcss",
+        },
+    },
+    build: {
+        format: "directory",
+        assets: "_assets",
+    },
 });
